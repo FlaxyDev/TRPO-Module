97a9e92d2d8faa112049f96e988ff70402ad8bd0
Diak Maksym <55993687+FlaxyDev@users.noreply.github.com>
2024-03-19 19:49:29 +0200

    Add files via upload

---
+++ MatrixOperations.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module
+{
+    internal class MatrixOperations
+    {
+        static double FunctionA(int i, int j)
+        {
+            return Math.Pow(Math.Tan(Math.Pow(j + 1, 3)), 2) / (i - i + 3) - Math.Pow(i, 2);
+        }
+        static double FunctionB(int j)
+        {
+            return Math.Pow(Math.Sin(Math.Pow(6 + j, 0.3)), 2) / (Math.Pow(j, 2) - 3);
+        }
+        public  static double [, ] CreateMaxtrixA(int n, int m)
+        {
+            double [,] matrixA = new double[n,m]; 
+            for(int i = 0; i <n;i++)
+            {
+                for(int j = 0; j< m; j++)
+                {
+                    matrixA[i, j] = FunctionA(i, j);
+                }
+            }
+            return matrixA;
+        }
+        public static double[] CreateVectorB(int n)
+        {
+            double[] vectorB = new double[n];
+            for(int j = 0; j <n; j++)
+            {
+                vectorB[j] = FunctionB(j);
+            }
+            return vectorB;
+        }
+        public static double[] MultiplyMatrix(double[,] matrixA, double[] vectorB)
+        {
+            int rows = matrixA.GetLength(0);
+            int cols = matrixA.GetLength(1);
+
+            if (cols != vectorB.Length)
+                throw new InvalidOperationException("Кількість стовпців у матриці повинна дорівнювати кількості елементів у векторі.");
+
+            double[] result = new double[rows];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                    result[i] += matrixA[i, j] * vectorB[j];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Console.InputEncoding = Encoding.Unicode;
+            int[] mValues = { 2, 4, 6, 8, 10 }; 
+            int[] nValues = { 2, 4, 6, 8, 10 }; 
+
+            foreach (int m in mValues)
+            {
+                foreach (int n in nValues)
+                {
+                    Console.WriteLine($"Проводимо експеримент для m={m}, l={n}:");
+                    Console.WriteLine("----------------------------------------");
+
+                    double[,] A = MatrixOperations.CreateMaxtrixA(m, n);
+                    double[] B = MatrixOperations.CreateVectorB(n);
+
+                    Stopwatch stopwatch = Stopwatch.StartNew();
+                    double [] C = MatrixOperations.MultiplyMatrix(A, B);
+                    stopwatch.Stop();
+
+                    Console.WriteLine($"Час виконання: {stopwatch.Elapsed.TotalMilliseconds} мілісекунд(и)\n");
+
+                    Console.WriteLine("Матриця A:");
+                    for (int i = 0; i < A.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < A.GetLength(1); j++)
+                        {
+                            Console.Write(A[i, j].ToString("F3") + "\t");
+                        }
+                        Console.WriteLine();
+                    }
+
+                    Console.WriteLine("\nВектор B:");
+                    for (int i = 0; i < B.GetLength(0); i++)
+                    {
+                        Console.Write(B[i].ToString("F3") + "\t");
+                    }
+
+                    Console.WriteLine("\nВектор C:");
+                    for (int i = 0; i < C.GetLength(0); i++)
+                    {
+                        Console.Write(C[i].ToString("F3") + "\t");
+                    }
+
+                    Console.WriteLine("\n\n");
+                }
+            }
+
+        }
+    }
+}
